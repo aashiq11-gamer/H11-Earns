9ade61d042dd7da7882ee789200bffe9fb15e1e2
aashiq11-gamer <786mhaashiq@gmail.com>
2025-01-30 06:42:11 +0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,16 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Firebase Configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBaqypWY8YWiT5fvse-lllrkhUSpYR1JQ8",
   authDomain: "h11-earnings.firebaseapp.com",
   databaseURL: "https://h11-earnings-default-rtdb.firebaseio.com",
   projectId: "h11-earnings",
-  storageBucket: "h11-earnings.firebasestorage.app",
+  storageBucket: "h11-earnings.appspot.com",
   messagingSenderId: "362605369321",
   appId: "1:362605369321:web:4d8e34c1eb11ec35c11d78",
   measurementId: "G-71115BPVKN"
 };
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.database();
