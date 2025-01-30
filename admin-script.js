79b2bbecfb739b05b6693809d954d9705fb813b2
aashiq11-gamer <786mhaashiq@gmail.com>
2025-01-30 00:17:00 -0500

    Create admin-script.js

---
+++ admin-script.js
@@ -0,0 +1,9 @@
+const approveDeposit = () => {
+  // Implement deposit approval logic
+  console.log("Deposit approved");
+};
+
+const approveWithdrawal = () => {
+  // Implement withdrawal approval logic
+  console.log("Withdrawal approved");
+};
