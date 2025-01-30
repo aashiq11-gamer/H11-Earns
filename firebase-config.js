31a37aa40463f4553d1f6c738fc5e716e1ea36f7
aashiq11-gamer <786mhaashiq@gmail.com>
2025-01-30 05:24:26 -0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,11 @@
-// Firebase Configuration (Your provided Firebase config)
 export const firebaseConfig = {
-  apiKey: "AIzaSyBaqypWY8YWiT5fvse-lllrkhUSpYR1JQ8",
+  apiKey: "AIzaSyBaqypWY8YWiT5fvse-lllrkhUSpYR1JQ8",  // Your API Key
   authDomain: "h11-earnings.firebaseapp.com",
   databaseURL: "https://h11-earnings-default-rtdb.firebaseio.com",
   projectId: "h11-earnings",
   storageBucket: "h11-earnings.firebasestorage.app",
   messagingSenderId: "362605369321",
   appId: "1:362605369321:web:4d8e34c1eb11ec35c11d78",
-  measurementId: "G-71115BPVKN"
+  measurementId: "G-71115BPVKN",
+  clientId: "362605369321-c1ltlq2bjkefpt8qo1q7qtl2rpg9hn07.apps.googleusercontent.com"  // Your Google Client ID
 };
