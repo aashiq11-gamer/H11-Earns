9cd0a194652cb5d9543ab30a2d90376973248ba5
aashiq11-gamer <786mhaashiq@gmail.com>
2025-01-30 01:45:37 -0500

    Update admin-script.js

--- admin-script.js
+++ admin-script.js
@@ -1,9 +1,9 @@
-const approveDeposit = () => {
-  // Implement deposit approval logic
-  console.log("Deposit approved");
-};
+import { getFirestore, collection, query, where, getDocs, updateDoc, doc } from 'https://www.gstatic.com/firebasejs/9.1.3/firebase-firestore.js';
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.1.3/firebase-app.js';
+import { firebaseConfig } from './firebase-config.js';
 
-const approveWithdrawal = () => {
-  // Implement withdrawal approval logic
-  console.log("Withdrawal approved");
-};
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+const approveDeposit = async () => {
+[_{{{CITATION{{{_1{](https://github.com/Unique111/writings/tree/6060f0fbac1948d369ea28730845ea59db03f478/network%2Fwebsocket.md)[_{{{CITATION{{{_2{](https://github.com/watchping/vue-course/tree/6a60dc019287a13859793f1ec7fef84dc41aa2b9/temp.md)[_{{{CITATION{{{_3{](https://github.com/knoxie87/knoxie87.github.io/tree/792eba59dc34cdb2cfb9f9220acea5cb1736db35/views%2Fadmin%2Fadmin_dashboard.php)
